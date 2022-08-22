667a4c9493f820d5b436c9a64d99b5d38043d775
Renan Souza <renansouza0122@gmail.com>
2022-08-22 20:14:47 -0300

    dark mode finished

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+const body = document.querySelector('body')
 const indicator = document.querySelector(".indicator")
 indicator.onclick = function(){
     indicator.classList.toggle('active')
+    body.classList.toggle('active')
 }
